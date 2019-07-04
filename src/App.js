f700d8aae33d7d9284445bfc7ba0439f3f3b3387
Bruno Lira <bruno.padovan@gmail.com>
2019-07-04 16:22:03 -0300

    collection preview and collection item components created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 import "./App.css";
 
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
