5b0612167815fad52d942fea93f3149cd265a70c
Bruno Lira <bruno.padovan@gmail.com>
2019-07-05 17:25:24 -0300

    api keys edited out

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,17 +2,9 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-const config = {
-  apiKey: "AIzaSyCV26zlqfxyer40Ab03viuJkQ1D963SSS4",
-  authDomain: "crwn-clothing-e75d5.firebaseapp.com",
-  databaseURL: "https://crwn-clothing-e75d5.firebaseio.com",
-  projectId: "crwn-clothing-e75d5",
-  storageBucket: "",
-  messagingSenderId: "1049089152541",
-  appId: "1:1049089152541:web:d7ac840e6f1d2a21"
-};
+import CONFIG from "./firebase.config";
 
-firebase.initializeApp(config);
+firebase.initializeApp(CONFIG);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
