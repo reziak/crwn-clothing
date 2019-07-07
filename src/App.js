6c49fe6dc9b4da9fb1c369523472a6baef68723d
Bruno Lira <bruno.padovan@gmail.com>
2019-07-07 00:10:38 -0300

    cart dropdown component created, cart reducer created

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   componentDidMount() {
     const { setCurrentUser } = this.props;
 
-    auth.unsubscribeFromAuth(async userAuth => {
+    auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,9 +6,12 @@ import { auth } from "../../firebase/firebase.utils";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
 import "./header.styles.scss";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, cartHidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -29,12 +32,15 @@ const Header = ({ currentUser }) => (
           Sign In
         </Link>
       )}
+      <CartIcon />
     </div>
+    {cartHidden ? null : <CartDropdown />}
   </div>
 );
 
 const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+  currentUser: state.user.currentUser,
+  cartHidden: state.cart.hidden
 });
 
 export default connect(mapStateToProps)(Header);
