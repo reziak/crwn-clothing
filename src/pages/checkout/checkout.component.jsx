52121cdcb373c0d7c53988c4cd4be563d6e67a09
Bruno Lira <bruno.padovan@gmail.com>
2019-07-11 00:38:11 -0300

    checkout item component gets upgraded with add and remove item funcionality

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -30,9 +30,13 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map(cartItem => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-    ))}
+    {cartItems.length ? (
+      cartItems.map(cartItem => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))
+    ) : (
+      <h2>Your cart is empty</h2>
+    )}
     <div className="total">
       <span>Total: ${total}</span>
     </div>
